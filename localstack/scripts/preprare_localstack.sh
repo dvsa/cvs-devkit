d700efe5c3e106a72c6ac35f2498eb27aefb100b
Tanio Artino <tartino@.deloitte.co.uk>
2021-08-02 16:35:42 +0100

    CVSB-19359 adding zip file to allow temporary demo on developers machines

---
+++ localstack/scripts/preprare_localstack.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+Banner()
+{
+   echo "======================================================"
+   echo "=       This is the localstack bootstrap             ="
+   echo "======================================================"
+}
+
+Check()
+{
+  response=(`curl --write-out "%{http_code}\n" --silent --output /dev/null "http://localhost:4566/health"`)
+
+  if [ ${response} -eq 200 ]; then
+     echo "Localstack is up."
+  else
+     echo "Localstack is down."
+     exit 1
+  fi
+}
+
+Bootstrap()
+{
+  echo "Setting zip files"
+  ./upload_all.sh -b
+  cd terraform
+  terraform init
+  terraform apply -auto-approve
+  cd ..
+  ./upload_reference_data.sh
+  ./seed_all.sh
+  ./upload_signature.sh
+  ./get_api_id.sh
+}
+
+Banner
+Check
+Bootstrap
