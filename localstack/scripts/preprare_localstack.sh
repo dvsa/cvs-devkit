d591db4abfbd0c6873d13c5395cd6360b78d1b94
Tanio Artino <tartino@.deloitte.co.uk>
2021-08-02 18:01:37 +0100

    CVSB-19359 adding zip file to allow temporary demo on developers machines

--- localstack/scripts/preprare_localstack.sh
+++ localstack/scripts/preprare_localstack.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-Banner()
+Help()
 {
    echo "======================================================"
    echo "=       This is the localstack bootstrap             ="
    echo "======================================================"
+   echo "options:"
+   echo "-c     check all dependencies                         "
+   echo "-b     bootstrap localstack                           "
+   echo
 }
 
 Check()
@@ -17,26 +21,46 @@ Check()
      echo "Localstack is down."
      exit 1
   fi
+  if ! command -v jq --version &> /dev/null
+  then
+    echo "jq could not be found"
+    exit
+  else
+    JQ_VERSION=(`jq --version`)
+    echo "jq is installed. version: $JQ_VERSION"
+  fi
   if ! command -v terraform --version &> /dev/null
   then
-    echo "terraform could not be found"
+    echo "jq could not be found"
     exit
+  else
+    TERRAFORM_VERSION=(`terraform version -json | jq -r '.terraform_version'`)
+    echo "terraform is installed. version: $TERRAFORM_VERSION"
   fi
 
   if ! command -v npm --version &> /dev/null
     then
       echo "node could not be found"
       exit
+  else
+    NODE_VERSION=(`npm --version`)
+    echo "node is installed. version: $NODE_VERSION"
   fi
   if ! command -v awslogs --version &> /dev/null
     then
       echo "awslogs could not be found"
       exit
+  else
+    AWSLOGS_VERSION=(`awslogs --version`)
+    echo "awslogs is installed. version: $AWSLOGS_VERSION"
   fi
   if ! command -v aws --version &> /dev/null
-    then
+  then
       echo "aws could not be found"
       exit
+  else
+    AWS_VERSION=(`aws --version`)
+    echo "aws is installed. version: $AWS_VERSION"
   fi
 }
 
@@ -57,6 +81,17 @@ Bootstrap()
   ./get_api_id.sh
 }
 
-Banner
-Check
-Bootstrap
+while getopts ":hcb" option; do
+   case ${option} in
+      h) Help
+         exit;;
+      c) Check
+         exit;;
+      b) Check
+         Bootstrap
+         exit;;
+      \?)
+         echo "Error: Invalid option"
+   esac
+done
+Help
